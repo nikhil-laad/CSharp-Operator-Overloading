1efc5f476f56ab7093fbff1c1d9cb6d17664a5e0
Nikhil Laad <nikhillaad12@gmail.com>
2019-04-23 18:29:07 +0530

    Add files via upload
    C# Operator Overloading

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+    public struct Complex {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+    }
+    class Program{
+        
+        static void Main(string[] args)
+            {
+                Complex a = new Complex() { Real = 1, Imaginary = 2 };
+                Complex b = new Complex() { Real = 1, Imaginary = 2 };
+                Complex c = a + b;
+            }
+
+    }
+}
