2cab051f7dac8fc5fdbdb456dce50ef57c4a64ab
Nikhil Laad <nikhillaad12@gmail.com>
2019-04-24 16:35:35 +0530

    Add files via upload
    Complete Program

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,15 @@ namespace ConsoleApplication1
     public struct Complex {
         public double Real { get; set; }
         public double Imaginary { get; set; }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            return new Complex
+            {
+                Real = c1.Real + c2.Real,
+                Imaginary = c1.Imaginary + c2.Imaginary
+            };
+        }
     }
     class Program{
         
@@ -18,7 +27,9 @@ namespace ConsoleApplication1
                 Complex a = new Complex() { Real = 1, Imaginary = 2 };
                 Complex b = new Complex() { Real = 1, Imaginary = 2 };
                 Complex c = a + b;
+            Console.WriteLine("Sum Real=" + c.Real + " Sum Imaginary=" + c.Imaginary);
+            Console.ReadKey();
             }
-
+        
     }
 }
